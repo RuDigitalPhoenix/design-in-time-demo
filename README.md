7673e2aa77b1260f30979b9210a723c19e1c1555
Rudigitalphoenix <write@rudigitalphoenix.com>
2016-12-24 19:53:47 +0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # design-in-time-demo
+
+Repository for developing some demo project of
+mysql+php DB connection
--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <html>
-<body>
-<h1>It works!</h1>
-</body>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Sergey Yeremin a.k.a. RuDigitalPhoenix">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Design In-Time DEMO CMS v0.1.1a login</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/custom-styles.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h4 class="form-signin-heading">Пожалуйста, авторизуйтесь для доступа к списку позиций!</h2>
+		<p>&nbsp;</p>
+        <label for="inputEmail" class="sr-only">Имя пользователя:</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Имя пользователя" required autofocus>
+        <label for="inputPassword" class="sr-only">Пароль:</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Запомнить меня
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+      </form>
+
+    </div> <!-- /container -->
+
+  </body>
 </html>
